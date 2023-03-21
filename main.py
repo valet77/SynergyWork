540c196470c02050b44d4480bf765b598190dfb6
valet77 <127590049+valet77@users.noreply.github.com>
2023-03-21 15:31:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ result = 0
 while (num := input("Введите число или ENTER для выхода - ")) != '':
     result = result + int(num)
 print(result)
-
-
+#  новый код
 print('Степанова Света изменение 1')
